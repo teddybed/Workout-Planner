e103b86cc8697efb2987593569aa3fd85a4b5a33
tewodros Bedilu <teddybedilu@gmail.com>
2025-06-17 05:44:00 -0700

    creating layout page for any navigation purpose and creating header page

---
+++ app/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Expo icons
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Ionicons name="arrow-back" size={28} color="#111" />
+      <Text style={styles.title}>Chris' Full Body</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f3f2f7',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginLeft: 10,
+    color: '#000',
+  },
+});
