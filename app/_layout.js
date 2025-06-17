cd4e9b0eceb6306bceb6c3b246ee2758a7738d59
tewodros Bedilu <teddybedilu@gmail.com>
2025-06-17 08:05:49 -0700

    wrap the layout page     <GestureHandlerRootView style={{ flex: 1 }}>

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }}  />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
